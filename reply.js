a28dd97be71f0d6c65c9df29651791abf4563acd
border-radius <pharshmaster@gmail.com>
2014-08-27 12:12:26 +0400

    ID is unique

--- reply.js
+++ reply.js
@@ -5,7 +5,12 @@ mongoose.connect('mongodb://localhost/bnw-talks');
 module.exports = mongoose.model('Reply', {
 	anonymous: Boolean,
 	date: Number,
-	id: String,
+	id: {
+    type: String,
+    index: {
+      unique: true
+    }
+  },
 	message: String,
 	num: Number,
 	replyto: String,
